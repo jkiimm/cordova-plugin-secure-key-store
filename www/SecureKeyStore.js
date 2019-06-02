5f7a4449b4a21eff15daca6f48b0c826d5b3c799
Jinyoung Kim <jkiimm9@gmail.com>
2019-06-02 16:40:45 +0900

    feat: return json if it is

--- www/SecureKeyStore.js
+++ www/SecureKeyStore.js
@@ -5,16 +5,26 @@ var SecureKeyStore = {
 	serviceName: "SecureKeyStore",
 
 	set: function(success, error, key, value) {
-		exec(success, error, this.serviceName, "set", [key, value]);
+		exec(success, errorWrapper(error), this.serviceName, "set", [key, value]);
 	},
 
 	get: function(success, error, key) {
-		exec(success, error, this.serviceName, "get", [key]);
+		exec(success, errorWrapper(error), this.serviceName, "get", [key]);
 	},
 
 	remove: function(success, error, key) {
-		exec(success, error, this.serviceName, "remove", [key]);
+		exec(success, errorWrapper(error), this.serviceName, "remove", [key]);
 	}
 };
 
 module.exports = SecureKeyStore;
+
+function errorWrapper (errorFn) {
+  return (error) => {
+    try {
+      errorFn(JSON.parse(error))
+    } catch (e) {
+      errorFn(error)
+    }
+  }
+}
